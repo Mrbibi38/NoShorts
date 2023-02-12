b285dd1c2ce8c3cf4bb197a744313fa6698f93ea
Lilian <mrbibi38@gmail.com>
2023-02-13 00:43:02 +0100

    An easier and more efficient way to remove shorts

--- src/removeSubscribtionShorts.js
+++ src/removeSubscribtionShorts.js
@@ -1,82 +1,23 @@
-// // Remove shorts from Youtube subscription page
+// Remove shorts from Youtube subscription page both in the list and grid display
 
+// The sleep function is a utility function that returns a promise that resolves after a specified amount of time has passed
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// The main function is an async function that removes the shorts videos
 async function main() {
-  for(let i = 0; i < 5; i++) {
-    await sleep(1000)
-    var shelf = document.querySelectorAll("ytd-item-section-renderer.style-scope.ytd-section-list-renderer")
+  await sleep(1000)
 
-    var shortsNode = []
-  
-    for (let i = 0; i < shelf.length; i++) {
-      let text = shelf[i].querySelector("a#video-title").href
-      if (text.includes("shorts")) {
-        shortsNode.push(shelf[i])
-      }
-    }
+    // Select all elements that match the CSS selector
+    var shortsList = document.querySelectorAll("ytd-item-section-renderer:has(a[href*=shorts])")
+    var shortsGrid = document.querySelectorAll("ytd-grid-video-renderer:has(a[href*=shorts])")
     
-    for (const node in shortsNode) {
-      shortsNode[node].parentNode.removeChild(shortsNode[node])
-    }
-  
-    console.log(shortsNode.length + " Shorts removed!");
-  }
+    // Remove all elements in shortsList
+    shortsList.forEach(short => short.remove())
+    // Remove all elements in shortsGrid
+    shortsGrid.forEach(short => short.remove())    
 }
 
+// Call the main function to remove the shorts
 main()
-startObserver()
-
-async function startObserver() {
-  await sleep(5000)
-
-  // Select the node that will be observed for mutations
-  const targetNode = document.getElementById('contents');
-
-  // Options for the observer (which mutations to observe)
-  const config = { attributes: true, childList: true, subtree: true };
-
-  // Callback function to execute when mutations are observed
-  const callback = function(mutationsList, observer) {
-      // Use traditional 'for loops' for IE 11
-      for(const mutation of mutationsList) {
-          if (mutation.type === 'childList') {
-              console.log('A child node has been added or removed.');
-          }
-          else if (mutation.type === 'attributes') {
-              console.log('The ' + mutation.attributeName + ' attribute was modified.');
-          }
-      }
-  };
-
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(callback);
-
-  // Start observing the target node for configured mutations
-  observer.observe(targetNode, config);
-}
-
-// while(document.querySelector("#contents") == null) {
-//   contents = document.querySelector("#contents")
-// }
-// contents.childElementCount
-// console.log(contents.childElementCount)
-
-
-// // .style-scope.ytd-video-renderer span[aria-label="Shorts"]
-
-// // .style-scope.ytd-thumbnail[overlay-style="SHORTS"]
-
-// // ytd-item-section-renderer.style-scope.ytd-section-list-renderer:has(.style-scope.ytd-thumbnail[overlay-style="SHORTS"]) {
-// //   border: 2px solid red;
-// //   display: none;
-// // }
-
-// // ytd-item-section-renderer:has(.style-scope.ytd-video-renderer span[aria-label="Shorts"]){
-// //   display: none !important;
-// // }
-
-
-// var shorts = document.querySelectorAll("ytd-item-section-renderer:has(.style-scope.ytd-video-renderer span[aria-label='Shorts'])")
